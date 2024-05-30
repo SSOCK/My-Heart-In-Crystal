0940cae7d5b70578f2dc396c845d96d1d3d7b1a5
kcwww <cwkim0321@gmail.com>
2024-05-30 19:57:57 +0900

    feat: add workflow && prettier && eslint && husky

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./SBIM/* ./output
+cp -R ./output ./SBIM/
