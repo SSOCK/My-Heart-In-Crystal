7e10549c1dafcabeed88e501a5f2fae38a60a3e6
kcwww <cwkim0321@gmail.com>
2024-05-31 18:13:12 +0900

    init: storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:storybook/recommended'],
   plugins: ['react', '@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2021,
