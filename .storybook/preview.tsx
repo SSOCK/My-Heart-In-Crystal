ee4c38b18a09b263e4ba180a061e3b89390101d0
kcwww <cwkim0321@gmail.com>
2024-06-07 02:35:31 +0900

    init: Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+import '../app/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-mode',
+    }),
+  ],
+};
+
+export default preview;
