e03800c54dcfa2d397dd58ea9bddcc9bdcfc9ff3
kcwww <cwkim0321@gmail.com>
2024-09-18 09:43:19 +0900

    test: add test modal component

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,12 @@
 import SnowGlobeCanvas from '@/shared/components/canvas/SnowGlobeCanvas';
+import TestModal from '@/shared/components/modals/testModal';
 
 const Home = () => {
   return (
     <>
+      <div style={{ zIndex: 1, position: 'relative' }}>
+        <TestModal />
+      </div>
       <SnowGlobeCanvas />
     </>
   );
---
+++ shared/components/modals/testModal.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+const TestModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TestModal;
