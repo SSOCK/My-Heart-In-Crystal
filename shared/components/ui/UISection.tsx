cfb170b8876dfff9835f42ffbc9765706db2dc63
kcwww <cwkim0321@gmail.com>
2024-09-21 10:00:35 +0900

    feat: add UI section components

---
+++ shared/components/ui/UISection.tsx
@@ -0,0 +1,23 @@
+const UISection = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section
+      style={{
+        minHeight: '100dvh',
+        zIndex: 1,
+        position: 'relative',
+        width: '100%',
+        overflow: 'hidden',
+        pointerEvents: 'none',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '5rem ',
+      }}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default UISection;
