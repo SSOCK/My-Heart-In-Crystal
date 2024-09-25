3fe8092b885853b44329da97e587ebd35d406698
kcwww <cwkim0321@gmail.com>
2024-09-25 12:02:21 +0900

    feat: add introduce Modal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 } satisfies Config;
 
 export default config;
