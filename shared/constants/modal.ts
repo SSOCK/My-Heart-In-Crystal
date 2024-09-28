50f7f74ac36627d180cad18a5fdd20e094e0dd5d
kcwww <cwkim0321@gmail.com>
2024-09-28 12:33:58 +0900

    feat: add modal constants

--- shared/constants/modal.ts
+++ shared/constants/modal.ts
@@ -1,4 +1,54 @@
-// enum
+export const MSG_COLOR = [
+  {
+    color: '#860A35',
+  },
+  {
+    color: '#860A35',
+  },
+  {
+    color: '#AF2655',
+  },
+  {
+    color: '#FF0000',
+  },
+  {
+    color: '#EE9322',
+  },
+  {
+    color: '#ffae00',
+  },
+  {
+    color: '#c8cf5c',
+  },
+  {
+    color: '#95af54',
+  },
+  {
+    color: '#1f831f',
+  },
+  {
+    color: '#016A70',
+  },
+  {
+    color: '#22668D',
+  },
+  {
+    color: '#0E21A0',
+  },
+  {
+    color: '#313866',
+  },
+  {
+    color: '#504099',
+  },
+  {
+    color: '#974EC3',
+  },
+  {
+    color: '#9F0D7F',
+  },
+] as const;
+
 const MODAL_TYPE = {
   GUEST: 'Guest',
   INTRODUCE: 'Intro',
