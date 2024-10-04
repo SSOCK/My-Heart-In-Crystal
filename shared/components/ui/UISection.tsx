537816f0c1868e892a8f6c4c10cdf96ccc7a969b
kcwww <cwkim0321@gmail.com>
2024-10-04 13:00:45 +0900

    feat: add toast message && share component

--- shared/components/ui/UISection.tsx
+++ shared/components/ui/UISection.tsx
@@ -1,10 +1,12 @@
 import ModalProvider from '@/shared/components/providers/ModalProvider';
+import { Toaster } from 'sonner';
 
 const UISection = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className="ui-section">
       {children}
       <ModalProvider />
+      <Toaster />
     </section>
   );
 };
