207ed5bddb5da2a8f0c299ed2fad51994033c750
kcwww <cwkim0321@gmail.com>
2024-10-04 13:20:55 +0900

    feat: add fullscreen component

---
+++ app/(protected)/main/_components/ui/FullScreen.tsx
@@ -0,0 +1,12 @@
+import { Fullscreen } from 'lucide-react';
+
+const FullScreen = () => {
+  return (
+    <Fullscreen
+      className="pointer-events-auto cursor-pointer hover:text-blue-500"
+      size={'2rem'}
+    />
+  );
+};
+
+export default FullScreen;
