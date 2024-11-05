c45dd49144d4807464d90c5d232ef08bec394d28
kcwww <cwkim0321@gmail.com>
2024-11-05 16:53:00 +0900

    temp: add visit make page

--- app/(public)/visit/[userId]/page.tsx
+++ app/(public)/visit/[userId]/page.tsx
@@ -8,6 +8,7 @@ import MessageCount from '@/app/(public)/visit/_components/MessageCount';
 import PreviousButton from '@/shared/components/ui/PreviousButton';
 import ArrowButtons from '../_components/ArrowButtons';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const VisitPage = ({ params }: { params: { userId: string } }) => {
   console.log(params.userId);
@@ -21,7 +22,12 @@ const VisitPage = ({ params }: { params: { userId: string } }) => {
         </div>
         <ArrowButtons />
         <Button className="pointer-events-auto w-full md:w-1/2 xl:w-1/3">
-          수정구슬 꾸미고 메세지 남기기
+          <Link
+            href="/visit/[userId]/message"
+            as={`/visit/${params.userId}/message`}
+          >
+            수정구슬 꾸미고 메세지 남기기
+          </Link>
         </Button>
       </UISection>
       <CrystalCanvas />
