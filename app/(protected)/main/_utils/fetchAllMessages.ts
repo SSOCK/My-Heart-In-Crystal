e3dfc6dca18dd368a800a953566860a8a434bf18
kcwww <cwkim0321@gmail.com>
2024-11-11 22:59:50 +0900

    feat: add fetch all message

---
+++ app/(protected)/main/_utils/fetchAllMessages.ts
@@ -0,0 +1,22 @@
+import { BACKEND_ROUTES } from '@/shared/constants/routes';
+import clientComponentFetch from '@/shared/utils/fetch/clientComponentFetch';
+
+const fetchMessages = async (user_id: string) => {
+  try {
+    const response = await clientComponentFetch(BACKEND_ROUTES.ALL_MESSAGES, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-User-Id': user_id,
+      },
+    });
+    if (!response.ok)
+      throw new Error('Failed to fetch message' + response.error);
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch message' + error);
+  }
+};
+
+export default fetchMessages;
