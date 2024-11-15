239931739894c278456ebe9bd68606d0662e1689
kcwww <cwkim0321@gmail.com>
2024-11-15 17:47:36 +0900

    feat: add ladingpage style

--- app/(landing)/_components/Header.tsx
+++ app/(landing)/_components/Header.tsx
@@ -1,7 +1,15 @@
 const Header = () => {
   return (
     <header>
-      <h1 className="font-bold text-yellow-300">수정 구슬 속 내 마음</h1>
+      <h1
+        className="text-3xl font-bold text-yellow-500"
+        style={{
+          textShadow:
+            '-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black',
+        }}
+      >
+        수정 구슬 속 내 마음
+      </h1>
     </header>
   );
 };
