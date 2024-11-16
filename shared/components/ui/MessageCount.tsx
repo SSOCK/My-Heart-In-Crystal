163931889e79cff8b73b70cf2263217b48ea4ad4
kcwww <cwkim0321@gmail.com>
2024-11-16 12:51:53 +0900

    style: message count component

--- shared/components/ui/MessageCount.tsx
+++ shared/components/ui/MessageCount.tsx
@@ -1,7 +1,16 @@
 const MessageCount = ({ count }: { count: number }) => {
   return (
     <div className="flex items-center gap-2">
-      <p style={{ fontSize: '1.5rem' }}>💌</p> <p>총 {count}개의 메시지</p>
+      <p style={{ fontSize: '1.5rem' }}>💌</p>{' '}
+      <p
+        className="flex justify-center text-white"
+        style={{
+          textShadow:
+            '-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black',
+        }}
+      >
+        총 {count}개의 메시지
+      </p>
     </div>
   );
 };
