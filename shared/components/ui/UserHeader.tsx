841e89537f8f91c77c7ef0c7528bc346578df4b4
kcwww <cwkim0321@gmail.com>
2024-11-16 12:48:44 +0900

    style: add user header

--- shared/components/ui/UserHeader.tsx
+++ shared/components/ui/UserHeader.tsx
@@ -3,7 +3,16 @@
 const UserHeader = ({ user }: { user: string }) => {
   return (
     <header className="flex gap-1">
-      <h1 className="user-header">{user}</h1> <p>님의 수정 구슬</p>
+      <h1 className="user-header">{user}</h1>{' '}
+      <p
+        className="flex items-center text-white"
+        style={{
+          textShadow:
+            '-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black',
+        }}
+      >
+        님의 수정 구슬
+      </p>
     </header>
   );
 };
