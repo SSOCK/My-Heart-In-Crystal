33691450bc02a5e568a9334445c023504f287506
kcwww <cwkim0321@gmail.com>
2024-11-16 18:58:03 +0900

    style: message modal

--- shared/components/modals/MessageModal.tsx
+++ shared/components/modals/MessageModal.tsx
@@ -12,6 +12,7 @@ import {
 import useModal from '@/shared/hooks/useModal';
 import MODAL_TYPE from '@/shared/constants/modal';
 import { MessageType } from '@/shared/types/message';
+import { formattedTime } from '@/shared/utils/time/formattedTime';
 
 const MessageModal = () => {
   const { isOpen, onClose, type, props } = useModal();
@@ -32,18 +33,53 @@ const MessageModal = () => {
         style={{ backgroundColor: letterColor }}
         onOpenAutoFocus={(e) => e.preventDefault()}
       >
-        <DialogHeader className="flex flex-col items-center justify-center gap-4">
-          <DialogTitle>{sender}</DialogTitle>
-          <DialogDescription className="flex flex-col items-center gap-2">
-            {sendAt}
-          </DialogDescription>
-          <div>{message}</div>
+        <DialogClose onClick={() => onClose()} className="flex justify-end">
+          <CircleX className="message-close" />
+        </DialogClose>
+
+        <div
+          className="text-center text-white"
+          style={{
+            textShadow:
+              '-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black',
+          }}
+        >
+          {message.split('\n').map((line, index) => (
+            <p key={index} className="text-xl">
+              {line}
+            </p>
+          ))}
+        </div>
+
+        <DialogHeader className="hidden">
+          <DialogTitle />
+          <DialogDescription />
         </DialogHeader>
 
-        <DialogFooter>
-          <DialogClose onClick={() => onClose()}>
-            <CircleX className="message-close" />
-          </DialogClose>
+        <DialogFooter
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          <div
+            className="text-xl"
+            style={{
+              color: 'rgba(254, 202, 202, 1)',
+              textShadow:
+                '-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black',
+            }}
+          >
+            {formattedTime(sendAt)}
+          </div>
+          <div
+            className="flex gap-2 text-xl"
+            style={{
+              color: 'rgba(34,197,94,1)',
+              textShadow:
+                '-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black',
+            }}
+          >
+            <p style={{ color: 'rgba(156, 163, 175, 1)' }}>From . </p>
+            {sender}
+          </div>
         </DialogFooter>
       </DialogContent>
     </Dialog>
