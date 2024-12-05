cd0e98b1cdc0c91159304fb219ec89dcf45602ef
kcwww <cwkim0321@gmail.com>
2024-12-05 18:44:13 +0900

    fix: formattedTiem

--- shared/utils/time/formattedTime.ts
+++ shared/utils/time/formattedTime.ts
@@ -3,9 +3,9 @@
 export const formattedTime = (time: string) => {
   const date = new Date(time);
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
   return `${year}-${month}-${day} ${hour}:${minute}`;
 };
