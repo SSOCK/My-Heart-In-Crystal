ae96c6b03e233eb55bc5435d5094d3f2bf561e59
kcwww <cwkim0321@gmail.com>
2024-12-10 16:02:27 +0900

    feat: add mainOnboarding msg

--- app/(protected)/main/_components/CrystalCanvas.tsx
+++ app/(protected)/main/_components/CrystalCanvas.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import dynamic from 'next/dynamic';
+import { useEffect, useState, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
@@ -14,10 +16,12 @@ import Glass from '@/shared/components/3dModels/Glass';
 import Snowflake from '@/shared/components/3dModels/Snowflake';
 import Ground from '@/shared/components/3dModels/Ground';
 import Environments from '@/shared/components/3dModels/Environment';
-
-import { Crystal } from '@/shared/types/crystal';
-import { Suspense } from 'react';
 import Loading from '@/shared/components/canvas/Loading';
+import CustomTooltip from '@/shared/components/ui/CustomTooltip';
+import { Crystal } from '@/shared/types/crystal';
+import { MAIN_ONBOARDING_STEPS } from '@/shared/constants/onBoading';
+
+const JoyRide = dynamic(() => import('react-joyride'), { ssr: false });
 
 const CrystalCanvas = ({
   data,
@@ -26,67 +30,109 @@ const CrystalCanvas = ({
   data: Crystal[];
   current: number;
 }) => {
+  const [run, setRun] = useState(false);
+
+  useEffect(() => {
+    const mainOnboarding = localStorage.getItem('mainOnboarding');
+    if (mainOnboarding !== 'completed') {
+      setRun(true);
+    }
+  }, []);
+
+  const handleJoyrideCallback = (data: { status: string; action: string }) => {
+    const { status, action } = data;
+
+    // Joyride 상태 확인: 'finished' 또는 'skipped'일 때 로컬 스토리지 저장
+    if (status === 'finished' || action === 'skip') {
+      localStorage.setItem('mainOnboarding', 'completed');
+      setRun(false); // 투어 중단
+    }
+  };
+
   return (
-    <section className="canvas-3d">
-      <Canvas
-        flat
-        linear
-        shadows={true}
-        camera={{ position: [16, 0, 0], fov: 100 }}
-      >
-        <Suspense fallback={<Loading />}>
-          <OrbitControls
-            target={[0, 0, 0]}
-            enablePan={false}
-            enableZoom={false}
-            maxPolarAngle={(Math.PI / 2 / 9) * 8}
-          />
-          <ambientLight intensity={1.5} color={'#ffffff'} />
-
-          <directionalLight
-            position={[30, 30, 10]} // x축을 더 멀리, y축을 더 높게, z축은 중앙으로
-            intensity={1} // 강도를 약간 높임
-            color={'#ffffff'}
-            castShadow={true}
-            shadow-mapSize-width={2048}
-            shadow-mapSize-height={2048}
-            shadow-camera-far={1000} // 더 멀리까지 보이도록
-            shadow-camera-near={1}
-            shadow-camera-left={-40}
-            shadow-camera-right={40}
-            shadow-camera-top={40}
-            shadow-camera-bottom={-40}
-          />
-
-          <Raycaster />
-          <Glass />
-          {Array.from({ length: 100 }, (_, i) => (
-            <Snowflake key={i} />
-          ))}
-          <Decorations crystal={data[current]._id} />
-          <MainDecoration
-            id={data[current].main_decoration_id}
-            color={data[current].main_decoration_color}
-          />
-          <Base />
-          <Bottom
-            id={data[current].bottom_decoration_id}
-            color={data[current].bottom_decoration_color}
-            title={data[current].title}
-          />
-          <Environments />
-          <Ground />
-          <EffectComposer>
-            <Bloom
-              mipmapBlur={true}
-              luminanceThreshold={0.1}
-              luminanceSmoothing={0.9}
-              intensity={0.2}
+    <>
+      <section className="canvas-3d">
+        <Canvas
+          flat
+          linear
+          shadows={true}
+          camera={{ position: [16, 0, 0], fov: 100 }}
+        >
+          <Suspense fallback={<Loading />}>
+            <OrbitControls
+              target={[0, 0, 0]}
+              enablePan={false}
+              enableZoom={false}
+              maxPolarAngle={(Math.PI / 2 / 9) * 8}
             />
-          </EffectComposer>
-        </Suspense>
-      </Canvas>
-    </section>
+            <ambientLight intensity={1.5} color={'#ffffff'} />
+
+            <directionalLight
+              position={[30, 30, 10]} // x축을 더 멀리, y축을 더 높게, z축은 중앙으로
+              intensity={1} // 강도를 약간 높임
+              color={'#ffffff'}
+              castShadow={true}
+              shadow-mapSize-width={2048}
+              shadow-mapSize-height={2048}
+              shadow-camera-far={1000} // 더 멀리까지 보이도록
+              shadow-camera-near={1}
+              shadow-camera-left={-40}
+              shadow-camera-right={40}
+              shadow-camera-top={40}
+              shadow-camera-bottom={-40}
+            />
+
+            <Raycaster />
+            <Glass />
+            {Array.from({ length: 100 }, (_, i) => (
+              <Snowflake key={i} />
+            ))}
+            <Decorations crystal={data[current]._id} />
+            <MainDecoration
+              id={data[current].main_decoration_id}
+              color={data[current].main_decoration_color}
+            />
+            <Base />
+            <Bottom
+              id={data[current].bottom_decoration_id}
+              color={data[current].bottom_decoration_color}
+              title={data[current].title}
+            />
+            <Environments />
+            <Ground />
+            <EffectComposer>
+              <Bloom
+                mipmapBlur={true}
+                luminanceThreshold={0.1}
+                luminanceSmoothing={0.9}
+                intensity={0.2}
+              />
+            </EffectComposer>
+          </Suspense>
+        </Canvas>
+      </section>
+      <JoyRide
+        tooltipComponent={CustomTooltip}
+        steps={MAIN_ONBOARDING_STEPS}
+        run={run}
+        disableOverlay={false}
+        disableCloseOnEsc={true}
+        disableOverlayClose={true}
+        hideCloseButton={true}
+        continuous={true}
+        showSkipButton={true}
+        spotlightClicks={false}
+        callback={handleJoyrideCallback}
+        styles={{
+          options: {
+            zIndex: 1000,
+          },
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        }}
+      />
+    </>
   );
 };
 
--- shared/constants/onBoading.ts
+++ shared/constants/onBoading.ts
@@ -20,3 +20,59 @@ export const LANDING_ONBOARDING_STEPS = [
     showBeacon: false,
   },
 ];
+
+export const MAIN_ONBOARDING_STEPS = [
+  {
+    target: '.main-onboarding-crystal-canvas',
+    content: '수정 구슬은 총 5개까지 보유할 수 있습니다.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-crystal-canvas',
+    content: '방문자들이 남긴 메세지는 이곳에 장식으로 보관되어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-showing',
+    content: '이 버튼을 통해 UI를 숨겨 예쁜 수정 구슬을 감상할 수 있어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-share-button',
+    content: '이 버튼을 통해 방문자들에게 내 수정구슬 링크를 공유할 수 있어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-hamburger',
+    content: '추가적으로 필요한 메뉴는 이곳에서 확인할 수 있어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+];
+
+export const VISITOR_ONBOARDING_STEPS = [
+  {
+    target: '.visitor-onboarding-crystal-canvas',
+    content:
+      '안녕하세요 수정 구슬 속 내 마음에 오신 것을 환영합니다!\n수정 구슬을 터치하면 수정 구슬 내부에 들어갈 수 있어요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.visitor-onboarding-crystal-canvas',
+    content:
+      '추가로 아래 내부장식들을 터치하면 다른 방문자들이 남긴 메시지도 볼 수 있어요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.visitor-onboarding-message',
+    content: '이곳을 클릭하여 새로운 메세지를 남겨보세요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+];
