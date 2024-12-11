3a201419c7bcae1da1afd0eb471eac3a6b9bfeed
kcwww <cwkim0321@gmail.com>
2024-12-11 14:10:15 +0900

    fix: landing float msg

--- shared/components/canvas/CrystalCanvas.tsx
+++ shared/components/canvas/CrystalCanvas.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { Canvas } from '@react-three/fiber';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
@@ -18,18 +18,20 @@ import Ground from '@/shared/components/3dModels/Ground';
 import CustomTooltip from '@/shared/components/ui/CustomTooltip';
 import Raycaster from '@/shared/components/canvas/Raycaster';
 import { LANDING_ONBOARDING_STEPS } from '@/shared/constants/onBoading';
+import Loading from './Loading';
 
 const JoyRide = dynamic(() => import('react-joyride'), { ssr: false });
 
 const CrystalCanvas = () => {
   const [run, setRun] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const landingOnboarding = localStorage.getItem('landingOnboarding');
-    if (landingOnboarding !== 'completed') {
+    if (isLoading && landingOnboarding !== 'completed') {
       setRun(true);
     }
-  }, []);
+  }, [isLoading]);
 
   const handleJoyrideCallback = (data: { status: string; action: string }) => {
     const { status, action } = data;
@@ -39,6 +41,10 @@ const CrystalCanvas = () => {
       localStorage.setItem('landingOnboarding', 'completed');
       setRun(false); // 투어 중단
     }
+  };
+
+  const loadingDone = () => {
+    setIsLoading(true);
   };
 
   return (
@@ -51,48 +57,50 @@ const CrystalCanvas = () => {
           shadows={true}
           camera={{ position: [16, 0, 0], fov: 100 }}
         >
-          <OrbitControls
-            target={[0, 0, 0]}
-            enablePan={false}
-            enableZoom={false}
-            maxPolarAngle={(Math.PI / 2 / 9) * 8}
-          />
-          <ambientLight intensity={1.5} color={'#ffffff'} />
-
-          <directionalLight
-            position={[30, 30, 10]} // x축을 더 멀리, y축을 더 높게, z축은 중앙으로
-            intensity={1} // 강도를 약간 높임
-            color={'#ffffff'}
-            castShadow={true}
-            shadow-mapSize-width={2048}
-            shadow-mapSize-height={2048}
-            shadow-camera-far={1000} // 더 멀리까지 보이도록
-            shadow-camera-near={1}
-            shadow-camera-left={-40}
-            shadow-camera-right={40}
-            shadow-camera-top={40}
-            shadow-camera-bottom={-40}
-          />
-          <Raycaster />
-          <Glass />
-          {Array.from({ length: 100 }, (_, i) => (
-            <Snowflake key={i} />
-          ))}
-          <Decorations />
-          <MainDecoration />
-          <Base />
-          <Bottom />
-          <Ground />
-          <Environments />
-
-          <EffectComposer>
-            <Bloom
-              mipmapBlur={true}
-              luminanceThreshold={0.1}
-              luminanceSmoothing={0.9}
-              intensity={0.2}
+          <Suspense fallback={<Loading loadingDone={loadingDone} />}>
+            <OrbitControls
+              target={[0, 0, 0]}
+              enablePan={false}
+              enableZoom={false}
+              maxPolarAngle={(Math.PI / 2 / 9) * 8}
             />
-          </EffectComposer>
+            <ambientLight intensity={1.5} color={'#ffffff'} />
+
+            <directionalLight
+              position={[30, 30, 10]} // x축을 더 멀리, y축을 더 높게, z축은 중앙으로
+              intensity={1} // 강도를 약간 높임
+              color={'#ffffff'}
+              castShadow={true}
+              shadow-mapSize-width={2048}
+              shadow-mapSize-height={2048}
+              shadow-camera-far={1000} // 더 멀리까지 보이도록
+              shadow-camera-near={1}
+              shadow-camera-left={-40}
+              shadow-camera-right={40}
+              shadow-camera-top={40}
+              shadow-camera-bottom={-40}
+            />
+            <Raycaster />
+            <Glass />
+            {Array.from({ length: 100 }, (_, i) => (
+              <Snowflake key={i} />
+            ))}
+            <Decorations />
+            <MainDecoration />
+            <Base />
+            <Bottom />
+            <Ground />
+            <Environments />
+
+            <EffectComposer>
+              <Bloom
+                mipmapBlur={true}
+                luminanceThreshold={0.1}
+                luminanceSmoothing={0.9}
+                intensity={0.2}
+              />
+            </EffectComposer>
+          </Suspense>
         </Canvas>
       </section>
       <JoyRide
